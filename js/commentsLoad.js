0fd0e162e40385cd4f1ccd881fb93671f79f2ab1
Zachary Guard <zacharyguard@gmail.com>
2015-07-10 21:56:50 +0800

    Add voting

--- js/commentsLoad.js
+++ js/commentsLoad.js
@@ -34,6 +34,31 @@ function loadComments(data) {
                     document.body.appendChild(elemContainer);
                     document.body.classList.add("pen-opened");
 
+                    var VOAT_BUTTONS_HTML = "<div class='pen_vote'>\
+                    <button class='pen_vote_button pen_vote--up'>Up</button>\
+                    <button class='pen_vote_button pen_vote--down'>Down</button>\
+                    </div>";
+
+                    elemContainer.addEventListener("click", function(e) {
+                        if (e.target.classList.contains("pen_vote_button")) {
+                            if (e.target.classList.contains("pen_vote--up")) {
+                                direction = 1;
+                            } else if (e.target.classList.contains("pen_vote--down")) {
+                                direction = -1;
+                            }
+                            var type = (e.target.parentElement.parentElement.classList.contains("pen_info")
+                                ? "submission" : "comment");
+
+                            var id = e.target.parentElement.parentElement.dataset.id;
+
+                            v.request("v1/vote/" + type + "/" + id + "/" + direction + "?revokeOnRevote=true", { verb: "POST" }, function(r) {
+                                console.log(r);
+                            });
+
+                            e.target.classList.toggle("highlighted");
+                        }
+                    })
+
                     /* get submission info */
 
                     console.log("requesting", "v1/submissions/" + id);
@@ -45,10 +70,11 @@ function loadComments(data) {
                             var date = new Date(r.data.date);
                             var subverse = r.data.subverse;
                             var author = r.data.userName;
-                            elemSubmissionInfo.innerHTML = "<h1 class='pen_info_title'>" + title + "</h1>\
+                            elemSubmissionInfo.innerHTML = VOAT_BUTTONS_HTML + "<h1 class='pen_info_title'>" + title + "</h1>\
                             <div class='pen_info_author'>" + author + "</div>\
                             <time class='pen_info_time'>" + date + "</time>\
                             <div class='pen_info_subverse'>/v/" + subverse + "</div>";
+                            elemSubmissionInfo.dataset.id = id;
                         }
                     });
 
@@ -64,7 +90,7 @@ function loadComments(data) {
 
                                 var elemComment = elem("div", ".pen_comment");
                                 elemComment.dataset.id = datum.id;
-                                elemComment.innerHTML = "<div class='pen_comment_content'></div>\
+                                elemComment.innerHTML = VOAT_BUTTONS_HTML + "<div class='pen_comment_content'></div>\
                                 <div class='pen_comment_author'></div><time class='pen_comment_time'></time>";
 
                                 elemComment.querySelector(".pen_comment_content").innerHTML = datum.formattedContent;
